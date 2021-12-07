fdd80db0982bb2d164c4314f52322590fe66cdf0
SeanIvers <seanivers55@gmail.com>
2021-12-07 13:23:24 -0500

    Add remove node function

--- linked_lists.py
+++ linked_lists.py
@@ -25,11 +25,13 @@ class LinkedList:
     def get_head_node(self):
         return self.head_node
 
+    # Inserts a value at the beginning
     def insert_beginning(self, new_value):
         new_node = Node(new_value)
         new_node.set_next_node(self.head_node)
         self.head_node = new_node
 
+    # Shows string representation of the list
     def stringify_list(self):
         string = ""
         current_node = self.head_node
@@ -37,3 +39,14 @@ class LinkedList:
             string += str(current_node.get_value()) + "\n"
             current_node = current_node.get_next_node()
         return string
+
+    def remove_node(self, value_to_remove):
+        current_node = self.head_node
+        if self.head_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            current_node = current_node.get_next_node()
+            while current_node != None:
+                if current_node.get_next_node().get_value() == value_to_remove:
+                    current_node.set_next_node(current_node.next_node.get_next_node())
+                    current_node = None
