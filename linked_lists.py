0dd164cab8d3aac936ef9b242382a0c4bf02faa7
SeanIvers <seanivers55@gmail.com>
2021-12-07 12:47:48 -0500

    Add linked list class

--- linked_lists.py
+++ linked_lists.py
@@ -17,3 +17,10 @@ class Node:
     def set_next_node(self, next_node):
         self.next_node = next_node
 
+# Linked List Class
+class LinkedList:
+    def __init__(self, value=None):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
